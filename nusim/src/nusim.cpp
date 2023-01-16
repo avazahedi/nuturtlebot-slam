94b934153627d3363ac1c7dc7f6a5279db8cd475
Ava Zahedi <AvaZahedi2023@u.northwestern.edu>
2023-01-16 16:19:09 -0600

    through Task C.2.3 builds without errors

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -5,7 +5,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 // #include "std_msgs/msg/string.hpp"
-#include "std_msgs/msg/UInt64.hpp"
+#include "std_msgs/msg/u_int64.hpp"
 
 using namespace std::chrono_literals;
 
@@ -20,10 +20,13 @@ class MinimalPublisher : public rclcpp::Node
     {
         this->declare_parameter("rate", 200);  // default to 200 Hz
 
-        timestep_pub_ = this->create_publisher<std_msgs::msg::String>("~timestep", 10);
+        int rate =
+          this->get_parameter("rate").get_parameter_value().get<int>();
+
+        timestep_pub_ = this->create_publisher<std_msgs::msg::UInt64>("~timestep", 10);
 
         timer_ = this->create_wall_timer(
-        500ms, std::bind(&MinimalPublisher::timer_callback, this));
+        std::chrono::milliseconds(1000/rate), std::bind(&MinimalPublisher::timer_callback, this));
     }
 
   private:
@@ -31,12 +34,12 @@ class MinimalPublisher : public rclcpp::Node
     {
         auto message = std_msgs::msg::UInt64();
         message.data = count_++;
-        RCLCPP_INFO(this->get_logger(), "Publishing timestep ", count);
+        RCLCPP_INFO(this->get_logger(), "Publishing timestep %u", count_);
         timestep_pub_->publish(message);
     }
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::UInt64>::SharedPtr timestep_pub_;
-    size_t count_;
+    unsigned int count_ = 0;
 };
 
 int main(int argc, char * argv[])
