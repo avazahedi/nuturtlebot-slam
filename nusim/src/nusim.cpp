8f5bd69f67fb2e66525534a397f95f70f2c71056
Ava Zahedi <AvaZahedi2023@u.northwestern.edu>
2023-01-18 14:47:32 -0600

    added teleport service and initial robot pose. checked that reset does in fact restore the robot to initial location. Task C.3 done

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -10,6 +10,7 @@
 #include "tf2/LinearMath/Quaternion.h"
 #include "tf2_ros/transform_broadcaster.h"
 #include "turtlesim/msg/pose.hpp"
+#include "nusim/srv/teleport.hpp"
 
 using namespace std::chrono_literals;
 
@@ -20,13 +21,18 @@ class NUSim : public rclcpp::Node
 {
   public:
     NUSim()
-    : Node("nusim"), count_(0)
+    : Node("nusim"), x0{0}, y0{10}, theta0{0}, count_{0}
     {
         // rate parameter
         declare_parameter("rate", 200);  // default to 200 Hz
 
         int rate =
           get_parameter("rate").get_parameter_value().get<int>();
+
+        // set initial values to pose
+        pose.x = x0;
+        pose.y = y0;
+        pose.theta = theta0;
 
         // timestep publisher
         timestep_pub_ = create_publisher<std_msgs::msg::UInt64>("~/timestep", 10);
@@ -36,13 +42,13 @@ class NUSim : public rclcpp::Node
             "~/reset", std::bind(&NUSim::reset_callback, this, 
             std::placeholders::_1, std::placeholders::_2));
 
+        // teleport service
+        teleport_srv_ = create_service<nusim::srv::Teleport>(
+            "~/teleport", std::bind(&NUSim::teleport_callback, this, 
+            std::placeholders::_1, std::placeholders::_2));
+
         // transform broadcaster
         tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
-
-        // turtle pose subscription
-        pose_sub_ = create_subscription<turtlesim::msg::Pose>("turtle1/pose", 10,
-                        std::bind(&NUSim::pose_callback, 
-                        this, std::placeholders::_1));
 
         // timer 
         timer_ = create_wall_timer(
@@ -54,7 +60,7 @@ class NUSim : public rclcpp::Node
     {
         auto message = std_msgs::msg::UInt64();
         message.data = count_;
-        RCLCPP_INFO(get_logger(), "Publishing timestep %u", count_);
+        // RCLCPP_INFO(get_logger(), "Publishing timestep %u", count_);
         timestep_pub_->publish(message);
         count_++;
 
@@ -66,6 +72,19 @@ class NUSim : public rclcpp::Node
         t.header.frame_id = "nusim/world";
         t.child_frame_id = "red/base_footprint";
 
+        // Set transform translation
+        t.transform.translation.x = pose.x;
+        t.transform.translation.y = pose.y;
+        t.transform.translation.z = 0.0;
+
+        // Set transform rotation in quaternion
+        tf2::Quaternion q;
+        q.setRPY(0, 0, pose.theta);
+        t.transform.rotation.x = q.x();
+        t.transform.rotation.y = q.y();
+        t.transform.rotation.z = q.z();
+        t.transform.rotation.w = q.w();
+
         // Send the transformation
         tf_broadcaster_->sendTransform(t);
 
@@ -75,21 +94,29 @@ class NUSim : public rclcpp::Node
                 std_srvs::srv::Empty::Response::SharedPtr)
     {
         count_ = 0;
+        pose.x = x0;
+        pose.y = y0;
+        pose.theta = theta0;
     }
 
-    void pose_callback(const std::shared_ptr<turtlesim::msg::Pose> msg)
+    void teleport_callback(const std::shared_ptr<nusim::srv::Teleport::Request> request,
+                            std::shared_ptr<nusim::srv::Teleport::Response>)
     {
-        pose.x = msg->x;
-        pose.y = msg->y;
-        pose.theta = msg->theta;
+        pose.x = request->x;
+        pose.y = request->y;
+        pose.theta = request->theta;
     }
 
+    double x0;
+    double y0;
+    double theta0;
+    unsigned int count_;
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::UInt64>::SharedPtr timestep_pub_;
-    unsigned int count_;
     rclcpp::Service<std_srvs::srv::Empty>::SharedPtr reset_srv_;
+    rclcpp::Service<nusim::srv::Teleport>::SharedPtr teleport_srv_;
     std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
-    rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr pose_sub_;
+    // rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr pose_sub_;
     turtlesim::msg::Pose pose;
 };
 
