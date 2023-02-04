d69e86b7b4710df2946ba5b07e27e0c691b888f4
Ava Zahedi <AvaZahedi2023@u.northwestern.edu>
2023-02-03 22:24:46 -0600

    Started Task E. Created launch file (E.5.1). Publishing to wheel_cmd. E.1.1-3 done

--- nuturtle_control/src/turtle_control.cpp
+++ nuturtle_control/src/turtle_control.cpp
@@ -30,6 +30,11 @@
 #include <vector>
 
 #include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "nuturtlebot_msgs/msg/wheel_commands.hpp"
+#include "nuturtlebot_msgs/msg/sensor_data.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
+#include "turtlelib/diff_drive.hpp"
 // #include "std_srvs/srv/empty.hpp"
 // #include "geometry_msgs/msg/transform_stamped.hpp"
 // #include "tf2/LinearMath/Quaternion.h"
@@ -42,21 +47,21 @@ class TurtleControl : public rclcpp::Node
 {
 public:
   TurtleControl()
-  : Node("turtle_control"), count_{0}
+  : Node("turtle_control")
   {
-    declare_parameter("wheel_radius", -1);
-    declare_parameter("track_width", -1);
+    declare_parameter("wheel_radius", -1.0);
+    declare_parameter("track_width", -1.0);
     declare_parameter("motor_cmd_max", -1);
-    declare_parameter("motor_cmd_per_rad_sec", -1);
+    declare_parameter("motor_cmd_per_rad_sec", -1.0);
     declare_parameter("encoder_ticks_per_rad", -1);
-    declare_parameter("collision_radius", -1);
+    declare_parameter("collision_radius", -1.0);
 
-    double radius = get_parameter("wheel_radius").get_parameter_value().get<double>();
-    double track = get_parameter("track_width").get_parameter_value().get<double>();
-    int motor_cmd_max = get_parameter("motor_cmd_max").get_parameter_value().get<int>();
-    int motor_cmd_prs = get_parameter("motor_cmd_per_rad_sec").get_parameter_value().get<int>();
-    int encoder_ticks = get_parameter("encoder_ticks_per_rad").get_parameter_value().get<int>();
-    double collision_rad = get_parameter("collision_radius").get_parameter_value().get<double>();
+    radius = get_parameter("wheel_radius").get_parameter_value().get<double>();
+    track = get_parameter("track_width").get_parameter_value().get<double>();
+    motor_cmd_max = get_parameter("motor_cmd_max").get_parameter_value().get<int>();
+    motor_cmd_prs = get_parameter("motor_cmd_per_rad_sec").get_parameter_value().get<double>();
+    encoder_ticks = get_parameter("encoder_ticks_per_rad").get_parameter_value().get<int>();
+    collision_rad = get_parameter("collision_radius").get_parameter_value().get<double>();
 
     if (radius==-1 || track==-1 || motor_cmd_max==-1 || motor_cmd_prs==-1 || encoder_ticks==-1 
         || collision_rad==-1)
@@ -65,25 +70,15 @@ public:
         throw(error);
     }
 
+    twist_sub_ = create_subscription<geometry_msgs::msg::Twist>("/cmd_vel", 10, 
+                 std::bind(&TurtleControl::twist_callback, this, std::placeholders::_1));
 
+    sensor_sub_ = create_subscription<nuturtlebot_msgs::msg::SensorData>("/sensor_data", 10,
+                  std::bind(&TurtleControl::sensor_callback, this, std::placeholders::_1));
 
-    // // obstacles publisher
-    // obstacles_pub_ = create_publisher<visualization_msgs::msg::MarkerArray>("~/obstacles", 10);
+    wheel_cmd_pub_ = create_publisher<nuturtlebot_msgs::msg::WheelCommands>("/wheel_cmd", 10);
 
-    // // timestep publisher
-    // timestep_pub_ = create_publisher<std_msgs::msg::UInt64>("~/timestep", 10);
-
-    // // reset service
-    // reset_srv_ = create_service<std_srvs::srv::Empty>(
-    //   "~/reset", std::bind(
-    //     &NUSim::reset_callback, this,
-    //     std::placeholders::_1, std::placeholders::_2));
-
-    // // teleport service
-    // teleport_srv_ = create_service<nusim::srv::Teleport>(
-    //   "~/teleport", std::bind(
-    //     &NUSim::teleport_callback, this,
-    //     std::placeholders::_1, std::placeholders::_2));
+    joint_states_pub_ = create_publisher<sensor_msgs::msg::JointState>("/joint_state", 10);
 
     // // transform broadcaster
     // tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
@@ -91,17 +86,14 @@ public:
 
     // timer
     timer_ = create_wall_timer(
-      std::chrono::milliseconds(5), std::bind(&TurtleControl::timer_callback, this));
+      std::chrono::milliseconds(500), std::bind(&TurtleControl::timer_callback, this));
   }
 
 private:
   /// \brief Timer callback that runs continuously on the provided frequency
   void timer_callback()
   {
-    // auto message = std_msgs::msg::UInt64();
-    // message.data = count_;
-    // timestep_pub_->publish(message);
-    // count_++;
+    wheel_cmd_pub_->publish(wheel_msg);
 
     // geometry_msgs::msg::TransformStamped t;
 
@@ -132,13 +124,62 @@ private:
 
   }
 
-  double x0;
-  double y0;
-  double theta0;
-  unsigned int count_;
+
+  void twist_callback(const geometry_msgs::msg::Twist & twist_data)
+  {
+    twist.w = twist_data.angular.z;
+    twist.x = twist_data.linear.x;
+    twist.y = twist_data.linear.y;
+
+    turtlelib::DiffDrive dd {track, radius};
+
+    wheels = dd.InverseKinematics(twist);
+
+    // convert to motor ticks/rad
+    wheel_msg.left_velocity = (int32_t)wheels.left/motor_cmd_prs;
+    wheel_msg.right_velocity = (int32_t)wheels.right/motor_cmd_prs;
+    if (wheel_msg.left_velocity > motor_cmd_max)
+    {
+      wheel_msg.left_velocity = motor_cmd_max;
+    }
+    else if (wheel_msg.left_velocity < -motor_cmd_max)
+    {
+      wheel_msg.left_velocity = -motor_cmd_max;
+    }
+    if (wheel_msg.right_velocity > motor_cmd_max)
+    {
+      wheel_msg.right_velocity = motor_cmd_max;
+    }
+    else if (wheel_msg.right_velocity < -motor_cmd_max)
+    {
+      wheel_msg.right_velocity = -motor_cmd_max;
+    }    
+  }
+
+  void sensor_callback(const nuturtlebot_msgs::msg::SensorData::SharedPtr sdata) const
+  {
+    
+  }
+
+  double radius;
+  double track;
+  int motor_cmd_max;
+  double motor_cmd_prs;
+  int encoder_ticks;
+  double collision_rad;
+
+  turtlelib::Twist2D twist;
+  turtlelib::WheelPosn wheels;
+  nuturtlebot_msgs::msg::WheelCommands wheel_msg;
+
+  
+
   rclcpp::TimerBase::SharedPtr timer_;
-//   rclcpp::Publisher<std_msgs::msg::UInt64>::SharedPtr timestep_pub_;
-//   rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr obstacles_pub_;
+  rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr twist_sub_;
+  rclcpp::Subscription<nuturtlebot_msgs::msg::SensorData>::SharedPtr sensor_sub_;
+  rclcpp::Publisher<nuturtlebot_msgs::msg::WheelCommands>::SharedPtr wheel_cmd_pub_;
+  rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_states_pub_;
+  
 //   rclcpp::Service<std_srvs::srv::Empty>::SharedPtr reset_srv_;
 //   rclcpp::Service<nusim::srv::Teleport>::SharedPtr teleport_srv_;
 //   std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
